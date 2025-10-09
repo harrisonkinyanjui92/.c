a7d3fcd4b1c7ac98c1999bc8aaefe913e878e9da
harrisonkinyanjui92 <harrisonkinyanjui980@gmail.com>
2025-10-09 19:13:44 +0300

    Add files via upload

---
+++ for_loop_numbers.c
@@ -0,0 +1,14 @@
+/*
+Name:Harrison Kinyanjui
+Reg No:CT100/G/26192/25
+Description:Print Numbers between (100-50)descending order
+*/
+#include <stdio.h>
+int main(){
+	int i;
+	for(i=100; i>=50; i--)
+	{
+		printf("%d\n",i);
+	}
+	return 0;
+}
